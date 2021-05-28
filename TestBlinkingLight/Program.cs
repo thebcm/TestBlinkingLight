0b86ec73054202c43784cfa3e96ba873d4e5b860
Michael Crawford <bcm@gravityjunction.com>
2021-05-27 22:12:52 -0400

    and switch logic

--- TestBlinkingLight/Program.cs
+++ TestBlinkingLight/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Device.Gpio;
+using System.Threading;
 
 namespace TestBlinkingLight
 {
+    
     class Program
     {
+        static int pin = 18;
+        private static bool _lightOn = false;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using GpioController controller = new();
+            controller.OpenPin(pin, PinMode.Output);
+            Console.WriteLine($"GPIO pin enabled: {pin}");
+
+            while (true)
+            {
+                string nextLightCondition = _lightOn ? "On":  "Off";
+                Console.WriteLine($"Turn light {nextLightCondition} Press Enter");
+                Console.ReadLine();
+                _lightOn = !_lightOn;
+                if (_lightOn)
+                {
+                    controller.Write(pin, PinValue.High);
+                }
+                else
+                {
+                    controller.Write(pin, PinValue.Low);
+                }
+                
+            }
         }
     }
 }
